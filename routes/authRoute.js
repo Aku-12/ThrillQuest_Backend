a601856d47bed8bc6a343b1eaae5646527314dee
Aku-12 <akashchaudhary205698@gmail.com>
2025-06-13 22:04:40 +0545

    auth route is added in routes

---
+++ routes/authRoute.js
@@ -0,0 +1,7 @@
+const { createAccount, login } = require('../controllers/userController') 
+
+const router = require('express').Router()
+
+router.post('/register', createAccount) 
+router.post('/login', login) 
+
