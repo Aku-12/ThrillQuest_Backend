8e46561fe7d6d58f48109bd70c35b6b2d883c630
Aku-12 <akashchaudhary205698@gmail.com>
2025-06-13 21:53:46 +0545

    userModel is implemented in models folder

---
+++ models/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const userModel = mongoose.Schema({
+    fName: {
+        type: String,
+        required: true,
+    },
+    lName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }, 
+    phoneNo: {
+        type: String,
+        required: true,
+    },
+    role:{
+        type: String,
+        enum: ["Customer", "Admin"],
+        default: "Customer"
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('User', userModel)
