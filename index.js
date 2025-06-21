c2514add79a5d5c0fbe97ba96d1eac39eaf814e5
Aku-12 <akashchaudhary205698@gmail.com>
2025-06-21 23:10:05 +0545

    updated index.js and authorization file

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-const express = require('express')
-require('dotenv').config()
-const authRoute = require("./routes/authRoute")
-const cors = require('cors')
-
-const connectDatabase = require('./config/database')
+const express = require("express");
+require("dotenv").config();
+const authRoute = require("./routes/authRoute");
+const cors = require("cors");
+const adminUserRoute = require("./routes/admin/adminUserRoute");
+const adminActivitiesRoute = require("./routes/admin/adminActivitiesRoute")
+const connectDatabase = require("./config/database");
+const path = require("path")
 
 const corsOrigin = {
-  origin: "*"
-}
+  origin: "*",
+};
 
-connectDatabase()
+connectDatabase();
 
-const app = express()
+const app = express();
 
-app.use(cors(corsOrigin))
+app.use(cors(corsOrigin));
+app.use(express.json());
 
-app.use(express.json())
+// Serve uploaded images statically
+app.use("/uploads", express.static(path.join(__dirname, "uploads")))
 
-app.use('/api/auth', authRoute )
+app.use("/api/auth", authRoute);
+app.use("/api/admin", adminUserRoute);
+app.use("/api/admin/activities", adminActivitiesRoute);
+//  // <-- Mount activities route
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT ;
 
-app.listen(PORT, ()=> {
-  console.log(`Server running at port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
+});
---
+++ middlewares/authorizedUser.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken")
+const User = require("../models/userModel")
+ 
+exports.authenticateUser =async(req,res,next)=>{
+    
+    const authHeader = req.headers.authorization;
+    try{
+        if(!authHeader || !authHeader.startsWith("Bearer")){
+            return res.status(401).json({success:false,msg:"Access Denied"})
+        }
+        const token = authHeader.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const user = await User.findOne({"_id": decoded.userId})
+        if(!user){
+            return res.status(401).json({"success": false, "message": "Token mismatch"})
+        }
+        req.user = user;
+        next();
+    }catch(e){
+        return res.status(500).json({success:false,msg:"Internal Server token Error"})
+    }
+}
+ 
+exports.isAdmin =async(req,res,next)=>{
+    if(req.user && req.user.role === 'admin'){
+        next()
+    }else{
+        return res.status(403).json(
+            {"success": false, "message": "Admin privilage required"}
+        )
+    }
+}
