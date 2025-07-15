88fe1a3367b1fae89c80c0158e05b962e084be57
Aku-12 <akashchaudhary205698@gmail.com>
2025-07-15 16:15:15 +0545

    routes are implemented of admin guides route

---
+++ routes/admin/adminGuidesRoute.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const {
+  createGuide,
+  getGuides,
+  getGuideById,
+  updateGuide,
+  deleteGuide,
+  rateGuide
+} = require("../../controllers/admin/guidesController");
+const {
+  authenticateUser,
+  isAdmin,
+} = require("../../middlewares/authorizedUser");
+
+const router = express.Router();
+
+// CREATE Guide
+router.post("/", authenticateUser, isAdmin, createGuide);
+
+// GET All Guides with Search, Filter, Pagination
+router.get("/", authenticateUser, getGuides);
+
+// GET Single Guide by ID
+router.get("/:id", authenticateUser, getGuideById);
+
+// UPDATE Guide
+router.put("/:id", authenticateUser, isAdmin, updateGuide);
+
+// DELETE Guide
+router.delete("/:id", authenticateUser, isAdmin, deleteGuide);
+
+router.post("/:id/rate", authenticateUser, rateGuide);
+
+module.exports = router;
