3031d7fc17f348a8e5e5adcb52266d526ff749fb
Aku-12 <akashchaudhary205698@gmail.com>
2025-07-15 21:09:30 +0545

    routes are in index.js

--- index.js
+++ index.js
@@ -1,33 +1,13 @@
-const express = require("express");
 require("dotenv").config();
-const authRoute = require("./routes/authRoute");
-const cors = require("cors");
-const adminUserRoute = require("./routes/admin/adminUserRoute");
-const adminActivitiesRoute = require("./routes/admin/adminActivitiesRoute")
+const app = require("./app");
 const connectDatabase = require("./config/database");
-const path = require("path")
 
-const corsOrigin = {
-  origin: "*",
-};
+const PORT = process.env.PORT || 5050;
 
-connectDatabase();
-
-const app = express();
-
-app.use(cors(corsOrigin));
-app.use(express.json());
-
-// Serve uploaded images statically
-app.use("/uploads", express.static(path.join(__dirname, "uploads")))
-
-app.use("/api/auth", authRoute);
-app.use("/api/admin", adminUserRoute);
-app.use("/api/admin/activities", adminActivitiesRoute);
-//  // <-- Mount activities route
-
-const PORT = process.env.PORT ;
-
-app.listen(PORT, () => {
-  console.log(`Server running at port ${PORT}`);
-});
+// Don't connect to real MongoDB if in test mode
+if (process.env.NODE_ENV !== "test") {
+  connectDatabase();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
