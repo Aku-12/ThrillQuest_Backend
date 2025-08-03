4b5ee85cc8983fae8abc696a9da8d41b84bda16d
Aku-12 <akashchaudhary205698@gmail.com>
2025-08-03 21:57:05 +0545

    Modify userModel to support additional profile fields

--- models/userModel.js
+++ models/userModel.js
@@ -1,36 +1,42 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const userModel = mongoose.Schema({
-    fName: {
-        type: String,
-        required: true,
+  fName: {
+    type: String,
+    required: true,
+  },
+  lName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phoneNo: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["customer", "admin"],
+    default: "customer",
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  profileImage: {
+    type: String,
+    default: "",
+  },
+  favorites: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Activity",
     },
-    lName: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    }, 
-    phoneNo: {
-        type: String,
-        required: true,
-    },
-    role:{
-        type: String,
-        enum: ["customer", "admin"],
-        default: "customer"
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    profileImage: {
-        type: String,
-        default: ""
-    }
-})
+  ],
+});
 
-module.exports = mongoose.model('User', userModel)
+module.exports = mongoose.model("User", userModel);
