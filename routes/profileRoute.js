c625ce0d0ab2592e73f3272da75fcc25bd095da8
Aku-12 <akashchaudhary205698@gmail.com>
2025-08-03 21:57:05 +0545

    Update profileRoute with new endpoints and middleware

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -1,21 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Import controller functions
-const {  
-    getUserProfile, 
-    changePassword, 
-    updateUserProfile 
-} = require('../controllers/userController');
+const {
+  getUserProfile,
+  changePassword,
+  updateUserProfile,
+  getMyFavorites,
+  addFavoriteBooking,
+  removeFavoriteBooking,
+} = require("../controllers/userController");
 
 // Import middlewares
-const { authenticateUser } = require('../middlewares/authorizedUser');
-const { single } = require('../middlewares/fileUpload'); // Correctly import the single upload middleware
+const { authenticateUser } = require("../middlewares/authorizedUser");
+const { single } = require("../middlewares/fileUpload");
 
 router.get("/fetch", authenticateUser, getUserProfile);
 
-router.put('/update/:userId', single('profileImage'), updateUserProfile);
+router.put("/update/:userId", single("profileImage"), updateUserProfile);
 
 router.put("/change-password", authenticateUser, changePassword);
+
+router.get("/get-favorites", authenticateUser, getMyFavorites);
+router.post("/add-favorite", authenticateUser, addFavoriteBooking);
+router.delete(
+  "/remove-favorite/:activityId",
+  authenticateUser,
+  removeFavoriteBooking
+);
 
 module.exports = router;
